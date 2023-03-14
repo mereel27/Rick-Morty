693028ae7bb9e18fff4f7d4ef4e892ac9c6e0e3d
mereel27 <eddie2705@gmail.com>
2023-03-14 22:59:23 +0100

    .

--- netlify/functions/userInfo.js
+++ netlify/functions/userInfo.js
@@ -1,5 +1,5 @@
 
-module.exports.handler = async (event, context) => {
+module.exports.handler = async (event) => {
   try {
     const { token } = event.queryStringParameters;
     const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?token=${token}`, {
@@ -10,8 +10,8 @@ module.exports.handler = async (event, context) => {
     });
     const jsonData = await response.json();
     return {
-      /* ok: response.ok, */
-      statusCode: 200/* response.status */,
+      ok: response.ok,
+      statusCode: response.status,
       body: JSON.stringify(jsonData),
     }
   } catch (err) {
